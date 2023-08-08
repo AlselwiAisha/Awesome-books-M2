d4a08c02ac418b81ce6224f821c2357ca1c796ed
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2023-08-08 10:37:40 +0300

    Update script.js

--- Js/script.js
+++ Js/script.js
@@ -2,8 +2,10 @@ const book = document.querySelector(".book");
 const form = document.querySelector("#form");
 const titl = document.querySelector("#title");
 const author = document.querySelector("#author");
-
+const removeButtons = document.querySelectorAll('.remove');
+const data = JSON.parse(localStorage.getItem('booksData')) || [];
 const booksArr = [];
+
 book.innerHTML = "";
 
 /*---------------Add Books List----------------*/
@@ -20,19 +22,16 @@ function addBooksList(id, title, authr) {
 
 /*----------------Save data to local storage----------------*/
 form.addEventListener('submit', () => {
-    const data = JSON.parse(localStorage.getItem('booksData')) || [];
     const bookData = {
         title_: titl.value,
         author_: author.value,
-    }
-        ;
+    };
     data.push(bookData);
     localStorage.setItem('booksData', JSON.stringify(data));
 });
 
 /*------------Get data from local storage and show it in HTML file------------*/
 function getBooksData() {
-    const data = JSON.parse(localStorage.getItem('booksData'));
     if (data) {
         let i = 0;
         data.forEach((d) => {
@@ -41,11 +40,10 @@ function getBooksData() {
         });
     }
 };
+
 window.addEventListener('load', () => {
     getBooksData();
 });
-
-const removeButtons = document.querySelectorAll('.remove');
 
 removeButtons.forEach((button) => {
     button.addEventListener('click', (e) => {
@@ -56,7 +54,6 @@ removeButtons.forEach((button) => {
 });
 
 function deleteData(index) {
-    const storeData = JSON.parse(localStorage.getItem('booksData'));
-    storeData.splice(index, 1);
-    localStorage.setItem(this.memAdrr, JSON.stringify(storeData));
+    data.splice(index, 1);
+    localStorage.setItem('booksData', JSON.stringify(data));
 }
