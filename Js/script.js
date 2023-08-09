6f9dacd6ef453dcc34cf29346f54b9d2b89c51a2
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2023-08-09 11:21:05 +0300

    Update script.js

--- Js/script.js
+++ Js/script.js
@@ -3,6 +3,11 @@ import AwesomeBooks from './module.js';
 const form = document.getElementById('myForm');
 const book = new AwesomeBooks('User', '#book-container');
 
+function deleteCookie(cookieName) {
+  document.cookie = `${cookieName}=; expires=Thu, 01 Jan 2023 00:00:00 UTC; path=/ ;domain=https://alselwiaisha.github.io/Awesome-books-M2/;`;
+}
+deleteCookie('myCookie');
+
 function handleBookStorage(e) {
   const { target } = e;
   if (target.matches('.remove')) {
